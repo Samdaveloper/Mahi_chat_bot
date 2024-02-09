ebbcef02cbb03c2aa3a36429d153f5daa8afa551
Sam_x_developer <129406748+Samdaveloper@users.noreply.github.com>
2024-02-09 20:31:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ async def start(client, message):
         
     else:
         buttons = [[
-            InlineKeyboardButton("❮ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ❯", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"")
+            InlineKeyboardButton("💝", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"")
         ]
         ]
         video = "https://telegra.ph/file/d8869bcc0797248090105.jpg"
